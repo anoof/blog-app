607280a4fbc124b4cac883d4aedbc42b06200ce2
Anoof Shaikh <anoofs@gmail.com>
2022-07-14 18:57:52 +0530

    restart

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({username: req.body.username});
-        !user && res.status(400).json("Wrong credentials!");
+        !user && res.status(400).json("Wrong credentials");
 
         const validated = await bcrypt.compare(req.body.password, user.password);
         !validated && res.status(400).json("Wrong credentials!");
